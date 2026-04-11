0766622c08696a548abc012928983dcc1ce2bcb6
StreamWInd114514 <3836884503@qq.com>
2026-04-11 23:14:12 +0800

    adding

--- src/testmod/TestMod.java
+++ src/testmod/TestMod.java
@@ -10,15 +10,7 @@ public class TestMod extends Mod {
 
     public TestMod() {
         Log.info("TestMod constructor called.");
-        Events.on(ClientLoadEvent.class, e -> {
-            // 延迟10帧后显示对话框，确保UI已经准备好
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("my_first_dialog");  // 对话框标题
-                dialog.cont.add("Welcome to my mod!").row();   // 显示文字
-                dialog.cont.button("OK", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
-        });
+        
     }
     @Override
     public void init() {
@@ -28,6 +20,5 @@ public class TestMod extends Mod {
     @Override
     public void loadContent() {
         Log.info("TestMod loadContent() called. Loading custom content...");
-        ModItems.load();
     }
 }
---
+++ src/testmod/content/Turret.java
@@ -0,0 +1,19 @@
+
+import arc.graphics.Color;
+import arc.graphics.g2d.Draw;
+import arc.math.Mathf;
+import arc.scene.ui.layout.Table;
+import arc.util.*;
+import mindustry.content.Fx;
+import mindustry.entities.Effect;
+import mindustry.entities.bullet.BasicBulletType;
+import mindustry.entities.bullet.BulletType;
+import mindustry.game.Team;
+import mindustry.gen.Building;
+import mindustry.gen.Unit;
+import mindustry.graphics.Drawf;
+import mindustry.graphics.Pal;
+import mindustry.ui.Fonts;
+import mindustry.world.blocks.defense.turrets.Turret;
+import mindustry.world.meta.BlockGroup;
+import mindustry.world.meta.Stat;
