2ae18c9e7d83d36b727fe007416d1aa547c96609
StreamWInd114514 <3836884503@qq.com>
2026-04-12 22:39:42 +0800

    adding7

--- src/testmod/content/Turret.java
+++ src/testmod/content/Turret.java
@@ -32,7 +32,7 @@ import mindustry.audio.SoundControl;
 
 public class Turret {
     public static Block PokerTurret;
-    public static Sound nonesound = nonesound;
+    public static Sound nonesound;
 
     public static void load() {
         PokerTurret = new PokerTurretBlock("PokerTurret");
