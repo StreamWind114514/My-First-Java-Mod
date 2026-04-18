9391407e3380ffaac5f4d302a46927dda663e7c2
StreamWInd114514 <3836884503@qq.com>
2026-04-18 15:57:42 +0800

    adding10

--- src/testmod/content/FloatingWord.java
+++ src/testmod/content/FloatingWord.java
@@ -4,22 +4,24 @@ import arc.graphics.g2d.Draw;
 import arc.graphics.g2d.Font;
 import arc.math.Interp;
 import arc.util.*;
+import arc.graphics.Color;
 import mindustry.entities.Effect;
 import arc.graphics.g2d.Lines;
 import mindustry.ui.Fonts;
 import mindustry.graphics.Pal;
 
-public static class FloatingWord {
+public class FloatingWord {
     public static Effect floatingText = new Effect(60f, 80f, e -> {
-        if (!(e.data instanceof Object[])) return;
-        Object[] data = (Object[]) e.data;
+        if (!(e.data instanceof Object[] data)) return;
         String text = (String) data[0];
         Color color = (Color) data[1];
-        Draw.color(color);
+        
         Font font = Fonts.outline;
         font.setColor(color);
-        float yOffset = (1f - e.fin(Interp.pow2Out)) * 24f;
-        font.draw(text, e.x, e.y + yOffset, 0.5f, Align.center);
-        Draw.color();
+        
+        float textWidth = font.getBounds(text).width;
+        float drawX = e.x - textWidth / 2; // 计算居中 X 坐标
+        float yOffset = e.fout(Interp.pow2Out) * 24f; // 上浮效果，距离为 24 像素
+        font.draw(text, drawX, e.y + yOffset);
     });
 }
