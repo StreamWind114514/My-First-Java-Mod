5a59add3159c1e508008439ec027b27a9da1b2ea
StreamWInd114514 <3836884503@qq.com>
2026-04-20 01:12:13 +0800

    adding11test

--- src/testmod/content/Turret.java
+++ src/testmod/content/Turret.java
@@ -63,6 +63,8 @@ public class Turret {
                 }
             }
         }
+        TextureRegion bregion = Core.atlas.find("poker/cardback");
+        cardImages.put("cardback", bregion);
     }
     public static void load() {
         loadSounds();
@@ -90,7 +92,33 @@ public class Turret {
             
         }
     }
-
+    protected static class CardBulletType extends BulletType {
+        private final TextureRegion cardRegion;
+        
+        public CardBulletType(TextureRegion region, float damage) {
+            super(8f, damage); // 速度8，伤害由参数决定
+            this.cardRegion = region;
+            keepVelocity = false;
+            hitEffect = Fx.hitBulletSmall;
+            smokeEffect = Fx.none;
+            shootEffect = Fx.none;
+            despawnEffect = Fx.none;
+            hitSound = cardhit;
+            sprite = region;
+            width = 32f;
+            height = 42f;
+            collides = true;
+            collidesGround = true;
+            collidesAir = true;
+            collidesTiles = false;
+            absorbable = false;
+        }
+        
+        @Override
+        public void draw(Bullet b) {
+            Draw.rect(cardRegion, b.x, b.y, b.rotation);
+        }
+    }
     protected static class PokerBulletType extends BulletType {
         PokerBulletType() {
             super(0f, 0f);
@@ -104,23 +132,25 @@ public class Turret {
         
         @Override
         public void init(Bullet b) {
-            skill();
-        }
-    
-        public void skill() {
             Seq<Object> result;
             String kind;
             int multiply;
+            int[] chooseCards;
             Seq<Card> card = new Seq<>();
             for (int i = 0; i < 5; i++) {
                 int value = Mathf.random(2, 14);
-                Suit suit = Suit.values()[Mathf.random(0, 3)];
+                Suit suit = Suit.values()[Mathf.random(0, 3)]; // 随机抽牌
                 card.add(new Card(value, suit));
             }
+            // 判断，获取
             result = analysisCards(card);
             kind = (String) result.get(0);
             multiply = (int) result.get(1);
+            chooseCards = (int[]) result.get(2).clone();
+            // 获取target
+            
         }
+    
         
         public Seq<Object> analysisCards(Seq<Card> cards) {
             Seq<Object> result = new Seq<>();
@@ -130,8 +160,10 @@ public class Turret {
                 values[i] = cards.get(i).value;
                 key[cards.get(i).value - 2] ++;
             }
-            Arrays.sort(values);
-            boolean straight = isStraight(values);
+            // 判断
+            int[] sortedValues = values.clone();
+            Arrays.sort(sortedValues);
+            boolean straight = isStraight(sortedValues);
             boolean onePair = isOnePair(key);
             boolean twoPair = isTwoPair(key);
             boolean threeKind = isThreeKind(key);
@@ -140,37 +172,102 @@ public class Turret {
             if (straight) {
                 result.add("Straight! (*10)");
                 result.add(10);
+                result.add(values);
             } else if (fullHouse) {
                 result.add("fullHouse! (*20)");
                 result.add(20);
+                result.add(values);
             } else if (fourKind) {
                 result.add("Four of a kind! (*40)");
                 result.add(40);
+                int temp = -1;
+                int[] choose = new int[5];
+                for (int i = 0; i < 5; i++) {
+                    if (key[i] == 4) {
+                        temp = i + 2;
+                        break;
+                    }
+                }
+                for (int i = 0; i < 5; i++) {
+                    choose[i] = (values[i] == temp) ? temp : 0;
+                }
+                result.add(choose);
             } else if (threeKind) {
                 result.add("Three of a kind! (*10)");
                 result.add(10);
+                int temp = -1;
+                int[] choose = new int[5];
+                for (int i = 0; i < 5; i++) {
+                    if (key[i] == 3) {
+                        temp = i + 2;
+                        break;
+                    }
+                }
+                for (int i = 0; i < 5; i++) {
+                    choose[i] = (values[i] == temp) ? temp : 0;
+                }
+                result.add(choose);
             } else if (twoPair) {
                 result.add("Two pair! (*5)");
                 result.add(5);
+                int temp1 = -1;
+                int temp2 = -1;
+                int[] choose = new int[5];
+                out:
+                for (int i = 0; i < 5; i++) {
+                    if (key[i] == 2) {
+                        temp1 = i + 2;
+                        for (int j = i + 1; j < 5; j++) {
+                            if (key[j] == 2) {
+                                temp2 = j + 2;
+                                break out;
+                            }
+                        }
+                    }
+                }
+                for (int i = 0; i < 5; i++) {
+                    choose[i] = (values[i] == temp1) ? temp1: (values[i] == temp2) ? temp2: 0;
+                }
+                result.add(choose);
             } else if (onePair) {
                 result.add("A pair! (*2)");
                 result.add(2);
+                int temp = -1;
+                int[] choose = new int[5];
+                for (int i = 0; i < 5; i++) {
+                    if (key[i] == 2) {
+                        temp = i + 2;
+                        break;
+                    }
+                }
+                for (int i = 0; i < 5; i++) {
+                    choose[i] = (values[i] == temp) ? temp : 0;
+                }
+                result.add(choose);
             } else {
                 result.add("High card! (*1)");
                 result.add(1);
+                int[] choose = new int[5];
+                int maxIndex = 0;
+                for (int i = 1; i < 5; i++) {
+                    if (values[i] > values[maxIndex]) maxIndex = i;
+                }
+                choose[maxIndex] = values[maxIndex];
+                result.add(choose);
             }
             
             
             return result;
 
         }
+        // 顺子
         private boolean isStraight (int[] card) {
-                for (int i = 0; i < 4; i++) {
-                    if (card[i + 1] != card[i] + 1) return false;
-                }
-                return true;
+            for (int i = 0; i < 4; i++) {
+                if (card[i + 1] != card[i] + 1) return false;
             }
-            
+            return true;
+        }
+        // 对子
         private boolean isOnePair(int[] cardKey) {
             int pair = 0;
             for (int i: cardKey) {
@@ -179,7 +276,7 @@ public class Turret {
             if (pair == 1) return true;
             return false;
         }
-            
+        // 两对
         private boolean isTwoPair(int[] cardKey) {
             int pair = 0;
             for (int i: cardKey) {
@@ -188,7 +285,7 @@ public class Turret {
             if (pair == 2) return true;
             return false;
         }
-            
+        // 三条
         private boolean isThreeKind(int[] cardKey) {
             int pair = 0;
             for (int i: cardKey) {
@@ -197,7 +294,7 @@ public class Turret {
             if (pair == 1) return true;
             return false;
         }
-        
+        // 四条
         private boolean isFourKind(int[] cardKey) {
             int pair = 0;
             for (int i: cardKey) {
@@ -206,7 +303,7 @@ public class Turret {
             if (pair == 1) return true;
             return false;
         }
-            
+        // 满堂红
         private boolean isFullHouse(int[] cardKey) {
             int pair2 = 0;
             int pair3 = 0;
@@ -218,7 +315,7 @@ public class Turret {
             return false;
         }
         
-        
+        // 卡牌类
         public static class Card {
             int value;
             Suit suit;
@@ -230,7 +327,7 @@ public class Turret {
                 return suit.name() + value;
             }
         }
-        
+        // 花色枚举
         enum Suit{
             CLUBS, DIAMONDS, HEARTS, SPADES
         }
