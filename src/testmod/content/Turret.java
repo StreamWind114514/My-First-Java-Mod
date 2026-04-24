a6306342b74d39fc98e9d9068b17a626ac829aac
StreamWInd114514 <3836884503@qq.com>
2026-04-25 00:45:30 +0800

    adding18

--- src/testmod/content/Turret.java
+++ src/testmod/content/Turret.java
@@ -225,7 +225,6 @@ public class Turret {
         
         public CardBulletType(TextureRegion region, float damage) {
             super(8f, damage); // 速度8，伤害由参数决定
-            float scale = 0.5f;
             this.cardRegion = region;
             keepVelocity = false;
             hitEffect = Fx.hitBulletSmall;
@@ -247,7 +246,8 @@ public class Turret {
         
         @Override
         public void draw(Bullet b) {
-            Draw.rect(cardRegion, b.x, b.y, CARD_W * scale, CARD_H * scale , b.rotation() - 90);
+            float scale = 0.5f;
+            Draw.rect(cardRegion, b.x, b.y, CARD_W * scale, CARD_H * scale, b.rotation() - 90);
         }
     }
     
